14677651e2851a433c31212858fcd3be1080b08d
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-07-16 10:57:27 +0900

    9-16

---
+++ HaByungno/12.js
@@ -0,0 +1,49 @@
+// 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 
+// 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 
+// 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+
+// 제한 조건
+// x는 1 이상, 10000 이하인 정수입니다.
+
+// function solution(x) {
+//     if (x % sumOfDigits(x) ===0){
+//         return `${x}의 모든 자릿수의 합은 ${sumOfDigits(x)}입니다. ${x}은 ${sumOfDigits(x)}로 나누어 떨어지므로 ${x}은 하샤드 수입니다.`
+//     } else {
+//         return `${x}의 모든 자릿수의 합은 ${sumOfDigits(x)}입니다. ${x}은 ${sumOfDigits(x)}로 나누어 떨어지지 않으므로 ${x}은 하샤드 수가 아닙니다..`
+//     }
+// }
+
+function solution(x) {
+    if (x % sumOfDigits(x) ===0){
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function sumOfDigits(n){
+    let answer = (String(n).split("").map(element => parseInt(element))).reduce((x, y) => {return x + y})
+    return answer
+}
+
+
+
+console.log(sumOfDigits(18))
+console.log()
+
+
+console.log(solution(18))
+console.log(solution(158))
+
+console.log()
+console.log()
+
+// 신기하게 짠 코드... 이게 한줄로 되네?
+function Harshad(n){
+    return !(n % (n + "").split("").reduce((a, b) => +b + +a ));
+}
+
+
+console.log(Harshad(10))
+console.log(Harshad(11))
---
+++ HaByungno/13.js
@@ -0,0 +1,26 @@
+// 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 
+// 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// n은 1 이상 100,000,000 이하인 자연수입니다.
+
+function solution(n) {
+    let answer = 0;
+    let ternary = n.toString(3)
+    ternary = ternary.split("").reverse().join("");
+    answer = parseInt(ternary, 3)
+
+    return answer;
+}
+
+
+console.log(solution(45))
+console.log(solution(125))
+
+
+
+
+
+const solution = (n) => {
+    return parseInt([...n.toString(3)].reverse().join(""), 3);
+}
